be5c2ff144b071efc7df2538a631384d412eeee4
Patryk Brzychcy <patryk.brzychcy@gmail.com>
2021-03-31 19:00:05 +0200

    Ulepszenie programu o obliczanie pola trojkata

--- main.c
+++ main.c
@@ -9,16 +9,23 @@ int main ()
 
     float a;
     float b;
+    float h;
     float pole_prostokata;
+    float pole_trojkata;
 
     printf ("Podaj dlugosc boku a - ");
     scanf ("%f", &a);
     printf ("Podaj dlugosc boku b - ");
     scanf ("%f", &b);
+    printf ("Podaj wysokosc h - ");
+    scanf ("%f", &h);
+
 
     pole_prostokata = ((a*b));
+    pole_trojkata = ((a*h))/2;
 
     printf("Pole prostokata wynosi:%.2f", pole_prostokata);
+    printf("Pole trojkata wynosi:%.2f", pole_trojkata);
 
 
      return 0;
